93af44b70165b3a07ecdefb07dc102a50bb3bd26
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-06-30 22:27:23 +0200

    Cover return types with Promise

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [1.0.1] - 30.06.2022
+
+### Fix
+
+- Cover return types with Promise
+
 ## [1.0.0] - 30.06.2022
 
 First version
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { Answer, QuizId, QuizInfo } from '@di-strix/quizee-types';
 
 export type QuizeeCloudFunction = 'getQuizeeList' | 'checkAnswers';
 
-export type GetQuizeeListFunction = () => QuizInfo[];
-export type CheckAnswersFunction = (data: { answers: Answer[]; quizId: QuizId }) => number;
+export type GetQuizeeListFunction = () => Promise<QuizInfo[]>;
+export type CheckAnswersFunction = (data: { answers: Answer[]; quizId: QuizId }) => Promise<number>;
